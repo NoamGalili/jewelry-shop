39efe0dbd90a5f57b076a67f9217e8f688b8be16
Chen-kaduri <chenkaduri@gmail.com>
2024-07-13 22:26:56 +0300

    continue changes to dynamic server
    ...

--- controllers/jewelries.js
+++ controllers/jewelries.js
@@ -1,8 +1,17 @@
 function getAlljewelries(req, res)
 {
-    res.render('jewelries.ejs')
+    res.render('jewelries.ejs',{
+                         jewelry: []
+                               })
 }
-
+function getAlljewelry(req, res)
+{
+    res.render('jewelry.ejs') //???
+}
+function deletejewelry(req, res)
+{
+    res.render('jewelry.ejs') //???
+}
 // making the function public
 module.exports = 
 {
