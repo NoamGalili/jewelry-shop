0f57101c364128aebbcf0aed7b8e1a62225e1ab8
talsadan0112 <165804185+talsadan0112@users.noreply.github.com>
2024-07-20 07:33:50 +0300

    created options
    created options when you press on 'Shop'

--- views/jewelry.js
+++ views/jewelry.js
@@ -5,5 +5,16 @@ document.getElementById('cart-button').addEventListener('click', function() {
     } else {
         cart.style.display = 'none';
     }
+  
+});
+
+document.getElementById('shop').addEventListener('click', function(event) {
+    event.preventDefault(); // מונע מהלינק לבצע ניווט
+    var options = document.getElementById('options');
+    if (options.classList.contains('hidden')) {
+        options.classList.remove('hidden');
+    } else {
+        options.classList.add('hidden');
+    }
 });
 
