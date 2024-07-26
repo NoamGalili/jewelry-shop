8b9750914d03ffb5ebf8de786be2e15a22d9f2b2
talsadan0112 <165804185+talsadan0112@users.noreply.github.com>
2024-07-27 02:55:34 +0300

    added a search bar

--- public/javascript/credit.js
+++ public/javascript/credit.js
@@ -87,4 +87,12 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     
     
+    document.addEventListener('DOMContentLoaded', function() {
+        const searchIcon = document.getElementById('searchIcon');
+        const searchBar = document.getElementById('searchBar');
+    
+        searchIcon.addEventListener('click', function() {
+            searchBar.classList.toggle('visible');
+        });
+    });
     
