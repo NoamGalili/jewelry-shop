c9c3be357c9cded2eef2f597e7a6bae1ef10c0ca
NoamGalili <>
2024-07-26 22:37:56 +0300

    fixed ejs's

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const server = express () //building the server
 server.use(express.static('public'))
 
 server.get("/", jewelriesController.getAlljewelries) //redirecting / to controller.
-server.get("/login", jewelriesController.getLogin)
+server.get("/login.ejs", jewelriesController.getLogin)
 server.get("/register.ejs", jewelriesController.getRegister)
 server.get("/payment.ejs", jewelriesController.getpayment)
 server.get("/credit.ejs", jewelriesController.getCredit)
 server.get("/deleteJewelry", jewelriesController.deleteJewelry)
-server.get("/jewelry", jewelriesController.getJewelry) 
+server.get("/jewelry.ejs", jewelriesController.getJewelry) 
 //server.get("/jewelry", jewelriesController.deletejewelry)
 server.listen(80) //listning on port 80 (might change to https and 443 if have time)
