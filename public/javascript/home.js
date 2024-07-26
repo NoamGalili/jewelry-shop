4b05568b9aa635539f2d77ac56d28c3b3a30b522
talsadan0112 <165804185+talsadan0112@users.noreply.github.com>
2024-07-27 00:00:13 +0300

    changes
    make the cart function working in all ejs pages,
    ajust the css

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -14,52 +14,9 @@ document.getElementById('shop').addEventListener('click', function(event) {
     }
 });
 
-document.getElementById('showNecklaces').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevents the link from navigating
-    document.getElementById('productGrid-necklaces').style.display = 'grid';
-    document.getElementById('productGrid-rings').style.display = 'none';
-    document.getElementById('productGrid-bracelets').style.display = 'none';
-    document.getElementById('productGrid-earrings').style.display = 'none';
-    document.getElementById('home').style.display = 'none';
-    document.getElementById('about').style.display = 'none';
-});
-
-document.getElementById('showRings').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevents the link from navigating
-    document.getElementById('productGrid-rings').style.display = 'grid';
-    document.getElementById('productGrid-bracelets').style.display = 'none';
-    document.getElementById('productGrid-earrings').style.display = 'none';
-    document.getElementById('productGrid-necklaces').style.display = 'none';
-    document.getElementById('home').style.display = 'none';
-    document.getElementById('about').style.display = 'none';
-});
-
-document.getElementById('showEarrings').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevents the link from navigating
-    document.getElementById('productGrid-earrings').style.display = 'grid';
-    document.getElementById('productGrid-bracelets').style.display = 'none';
-    document.getElementById('productGrid-necklaces').style.display = 'none';
-    document.getElementById('productGrid-rings').style.display = 'none';
-    document.getElementById('home').style.display = 'none';
-    document.getElementById('about').style.display = 'none';
-});
-
-document.getElementById('showBracelets').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevents the link from navigating
-    document.getElementById('productGrid-bracelets').style.display = 'grid';
-    document.getElementById('productGrid-earrings').style.display = 'none';
-    document.getElementById('productGrid-necklaces').style.display = 'none';
-    document.getElementById('productGrid-rings').style.display = 'none';
-    document.getElementById('home').style.display = 'none';
-    document.getElementById('about').style.display = 'none';
-});
 
 document.getElementById('home-link').addEventListener('click', function(event) {
     event.preventDefault(); // Prevents the link from navigating
-    document.getElementById('productGrid-necklaces').style.display = 'none';
-    document.getElementById('productGrid-rings').style.display = 'none';
-    document.getElementById('productGrid-earrings').style.display = 'none';
-    document.getElementById('productGrid-bracelets').style.display = 'none';
     document.getElementById('home').style.display = 'block';
     document.getElementById('about').style.display = 'block';
 });
