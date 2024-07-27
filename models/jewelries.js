99af1a130fa8c03caeddf4a46464365b8dcd98a8
NoamGalili <>
2024-07-28 00:43:50 +0300

    first db functions - doesnt work

--- models/jewelries.js
+++ models/jewelries.js
@@ -1,36 +1,52 @@
-
        
 
  async function getAllJewelries() //allowing the rest of the project to access the jewleries json 
  {
   const { MongoClient } = require('mongodb');
-  const uri  = 'mongodb://127.0.0.1:27017/';
+  const uri  = 'mongodb://0.0.0.0:27017/';
   const client = new MongoClient(uri);
   await client.connect();
-  const jewelries = await client.db('collection name').collection('<collection name>').find();
+  const jewelries = await client.db('NTLC').collection('Jewleries').find();
   return await jewelries.toArray()
  }
 
+// JewelryID
 /*
+-------DB Template-------
  function getAllJewelries() //allowing the rest of the project to access the jewleries json 
  {
   const { MongoClient } = require('mongodb');
   const uri  = 'mongodb://127.0.0.1:27017/';
   const client = new MongoClient(uri);
   await client.connect();
-  const jewelries = await client.db('collection name').collection('<collection name>').find();
+  const jewelries = await client.db('<DB name>').collection('<collection name>').find();
   return await jewelries.toArray()
  }
 */
 
- function getJewelry(id) //allowing the rest of the project to access the jewleries json 
+ async function getJewelry(id) //allowing the rest of the project to access the jewleries json 
  {
-    return jewelries.filter(Jewelry => Jewelry.id == id)[0];
+    const { MongoClient } = require('mongodb');
+    const uri  = 'mongodb://0.0.0.0:27017/';
+    const client = new MongoClient(uri);
+    await client.connect();
+    const jewelries = await client.db('NTLC').collection('Jewleries').find();
+    return await jewelries.toArray()
+
+    //return jewelries.filter(Jewelry => Jewelry.id == id)[0];
  }
 
+ async function getJewelryByID(jewelryID) 
+ {
+    const jewelryItem = await Jewelry.findOne({ jewelryID });
+    return jewelryItem;
+}
+ 
+ module.exports = getJewelryByID;
+ 
  function deleteJewelry(id)
  {
-   const indexToDelete= jewelries.findIndex(Jewelry => Jewelry.id ==id)
+   const indexToDelete= jewelries.findIndex(Jewelry => Jewelry.id == id)
    jewelries.splice(indexToDelete, 1)
  }
 
