6b6d5030ec5f274ec030bd6670aa55e30c43a944
NoamGalili <>
2024-07-30 18:53:52 +0300

    update

--- services/users.js
+++ services/users.js
@@ -1,33 +1,48 @@
 const User = require ('../models/users')
 
-const createUser = async (jewelryData) => 
+const createUser = async (userData) => 
+{
+    try 
     {
-      try 
-      {
-        const newUser = new Jewelry(
+        const salt = crypto.randomBytes(16).toString('hex');
+        const hash = await calculateHash(userData.UserPassword + salt);
+
+        const newUser = new User(
         {
-            UserID: jewelryData.JewelryID,
-            UserType: jewelryData.JewelryType,
-            UserColor: jewelryData.JewelryColor,
-            UserName: jewelryData.JewelryName,
-            UserPhoto: jewelryData.JewelryPhoto,
-            UserPrice: jewelryData.JewelryPrice,
-            UserDescription: jewelryData.JewelryDescription,
-            UserAddDate: jewelryData.JewelryAddDate || Date.now(),
-            UserAvaliableStock: jewelryData.JewelryAvaliableStock,
-            UserAmountOfOrders: 0
+            UserID: userData.UserID,
+            UserType: userData.UserType,
+            Username: userData.Username,
+            UserMail: userData.UserMail,
+            UserPasswordSHA256Hash: hash,
+            UserSalt: salt
         });
-    
+
         await newUser.save();
         return newUser;
-      }
-      
-      catch (error) 
-      {
+    }
+    
+    catch (error) 
+    {
         console.error('Error creating user:', error);
         throw error;
-      }
     }
+}
+
+async function calculateHash(message) 
+{
+        // Convert the message to a Uint8Array
+        const msgUint8 = new TextEncoder().encode(message);
+        
+        // Calculate the hash
+        const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+        
+        // Convert the hash to a hex string
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+        const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+        
+        return hashHex;
+}
+    
 
 module.exports = 
 {
