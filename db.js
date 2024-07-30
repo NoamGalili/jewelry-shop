3e819b91519fa49e951e9e6ea3ab1ed1f6f2ad83
NoamGalili <>
2024-07-30 23:16:33 +0300

    fixed warnings

--- db.js
+++ db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://0.0.0.0:27017/NTLC', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect('mongodb://0.0.0.0:27017/NTLC', {});
     console.log('Connected to the database.');
   } catch (err) {
     console.error('Database connection error:', err);
