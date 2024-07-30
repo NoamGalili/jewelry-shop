ccf49021ad5736bce131af087a859acfddb9aa44
NoamGalili <>
2024-07-30 14:50:58 +0300

    added create jewelry function

--- services/jewelries.js
+++ services/jewelries.js
@@ -1,22 +1,52 @@
-const Jewelry = require ('../models/jewelries')
+const Jewelry = require('../models/jewelries');
 
-async function getAllJewelries() //allowing the rest of the project to access the jewleries json 
-{   
-    return await Jewelry.find({});
+async function getAllJewelries() 
+{
+  return await Jewelry.find({});
 }
 
-const getJewelryByID = async (id) =>
+const getJewelryByID = async (id) => 
 {
-    return await Jewelry.findById(id);
-}
-const deleteJewelry = async (id) =>
-{
-    return await Jewelry.findById(id);
+  return await Jewelry.findById(id);
 }
 
-module.exports = 
+const deleteJewelry = async (id) => 
 {
-  getAllJewelries,
-  getJewelryByID,
-  deleteJewelry
+  return await Jewelry.findById(id);
+}
+
+const createJewelry = async (jewelryData) => 
+{
+  try 
+  {
+    const newJewelry = new Jewelry(
+    {
+      JewelryID: jewelryData.JewelryID,
+      JewelryType: jewelryData.JewelryType,
+      JewelryColor: jewelryData.JewelryColor,
+      JewelryName: jewelryData.JewelryName,
+      JewelryPhoto: jewelryData.JewelryPhoto,
+      JewelryPrice: jewelryData.JewelryPrice,
+      JewelryDescription: jewelryData.JewelryDescription,
+      JewelryAddDate: jewelryData.JewelryAddDate || Date.now(),
+      JewelryAvaliableStock: jewelryData.JewelryAvaliableStock,
+      JewelryAmountOfOrders: 0
+    });
+
+    await newJewelry.save();
+    return newJewelry;
+  }
+  
+  catch (error) 
+  {
+    console.error('Error creating jewelry:', error);
+    throw error;
+  }
+}
+
+module.exports = {
+    getAllJewelries,
+    getJewelryByID,
+    deleteJewelry,
+    createJewelry
 }
