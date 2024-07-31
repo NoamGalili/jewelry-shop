a3733f60d88dc3acd4711c3381f7b4774c6068f7
talsadan0112 <165804185+talsadan0112@users.noreply.github.com>
2024-07-31 11:14:43 +0300

    Update db.js
    change db.js

--- db.js
+++ db.js
@@ -1,16 +1,15 @@
-const mongoose = require('mongoose');
+ const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect('mongodb://192.168.50.50:27017/NTLC', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
+ const connectDB = async () => {
+     try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/NTLC', {
+   
     });
     console.log('Connected to the database.');
-  } catch (err) {
-    console.error('Database connection error:', err);
-    process.exit(1);
-  }
-};
+   } catch (err) {
+   console.error('Database connection error:', err);
+     process.exit(1);
+   }
+ };
 
-module.exports = connectDB;
+ module.exports = connectDB;
